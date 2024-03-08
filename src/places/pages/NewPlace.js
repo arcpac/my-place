15bd87490b7d553cf6a4eae1b2ef1b5824ed4640
antoncab <antonraphaelcaballes@gmail.com>
2024-03-08 23:02:01 +1100

    add input and validators

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,7 +1,19 @@
+import Button from "../../shared/components/Buttons/Button";
+import Input from "../../shared/components/Input/Input";
+import { VALIDATOR_REQUIRE } from "../../shared/utils/validators";
+
 const NewPlace = () => {
     return (
-        <div>
-            New place
+        <div className="container">
+
+            <div class="row justify-content-md-center">
+                <div className="col-auto">
+                    <form className="place-form">
+                        <Input type="text" placeholder="Location" label="Location" id="location" element="input" validators={[VALIDATOR_REQUIRE()]} errorText="Invalid input"/>
+                    </form>
+                </div>
+            </div>
+
         </div>
     )
 }
---
+++ src/shared/components/Input/Input.js
@@ -0,0 +1,56 @@
+import { useReducer } from "react";
+import { validate } from "../../utils/validators";
+
+const inputReducer = (state, action) => {
+    switch (action.type) {
+        case 'CHANGE':
+            return {
+                ...state,
+                value: action.val,
+                isValid: validate(action.val, action.validators)
+            };
+        case 'TOUCH':
+            return {
+                ...state,
+                isTouched: true,
+            }
+        default:
+            break;
+    }
+}
+
+const Input = (props) => {
+    const [inputState, dispatch] = useReducer(inputReducer, { value: '', isValid: false })
+
+    const changeHandler = event => {
+        dispatch({ type: 'CHANGE', val: event.target.value, validators: props.validators })
+    }
+    const touchHandler = event => {
+        dispatch({ type: 'TOUCH', isTouched: false })
+    }
+
+    const element =
+        props.element === 'input' ?
+            (
+                <input id={props.id} type={props.type} placeholder={props.placeholder} className={`form-control`} onChange={changeHandler} value={inputState.value}
+                    onBlur={touchHandler}
+                />
+            )
+            :
+            (
+                <textarea id={props.id} rows={props.row || 3} onChange={changeHandler} value={inputState.value} onBlur={touchHandler} />
+            );
+
+    return (
+        <div className={`form-control`}>
+            <label htmlFor={props.id}>{props.label}</label>
+            {element}
+
+            {!inputState.isValid && inputState.isTouched && <div class="alert alert-info" role="alert">
+                {props.errorText}
+            </div>}
+        </div>
+    )
+}
+
+export default Input;
